d488685e29ad3ac7c1b3214f5a5248b97544719a
Shaun <s-purslow@hotmail.com>
2021-08-23 14:47:52 -0600

    add input text, the header with amazon logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,27 @@
 import Image from 'next/image';
 
 const Header = () => {
-  return <header>
-    {/* Top Nav */}
-    <div>
-      <div>
-        <Image
-          src='https://links.papareact.com/f90'
-          width={150}
-          height={40}
-         />
-      </div>
-    </div>
-    {/* Bottom Nav */}
-    <div>
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+        <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
+          <Image
+            src='https://links.papareact.com/f90'
+            width={150}
+            height={40}
+            objectFit='contain'
+            className='cursor-pointer'
+          />
+        </div>
 
-    </div>
-  </header>;
+        {/* Search */}
+        <input type="text" />
+      </div>
+      {/* Bottom Nav */}
+      <div></div>
+    </header>
+  );
 };
 
 export default Header;
