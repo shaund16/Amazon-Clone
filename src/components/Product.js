f8841ab205fc7f18049064e71c8684f061211b1a
Shaun <s-purslow@hotmail.com>
2021-08-23 16:35:24 -0600

    style and pass the props into product component, add heroicon, a useState for the ratings stars about to setup

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,17 @@
-const Product = ({ id, title, price, description, category }) => {
-  return <div>
-    <p>{category}</p>
-  </div>;
+import Image from 'next/image';
+import { useState } from 'react';
+import { StarIcon } from '@heroicons/react/solid';
+
+const Product = ({ id, title, price, description, category, image }) => {
+  const [rating] = useState(2);
+  return (
+    <div>
+      <p>{category}</p>
+
+      <Image src={image} height={200} width={200} objectFit='contain' />
+      <h4>{title}</h4>
+    </div>
+  );
 };
 
 export default Product;
