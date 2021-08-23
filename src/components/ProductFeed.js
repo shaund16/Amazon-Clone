97fe46c9768aa6bb8eeb490d755c032900c81ede
Shaun <s-purslow@hotmail.com>
2021-08-23 16:28:53 -0600

    map through the products, destruction the products

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import Product from './Product';
 
-const ProductFeed = () => {
+const ProductFeed = ({ products }) => {
   return (
     <div>
-      
+      <h1>Products</h1>
+      {products.map(({ id, title, price, description, category, image }) => (
+        <Product
+          key={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProductFeed
+export default ProductFeed;
