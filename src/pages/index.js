19d7376ab6fcfeb6c9051658378ed86dfd8bb532
Shaun <s-purslow@hotmail.com>
2021-08-23 16:09:16 -0600

    render banner and add background color gray to div and style the main tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import ProductFeed from '../components/ProductFeed';
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -16,6 +17,7 @@ export default function Home() {
         <Banner />
 
         {/* Product Feed */}
+        <ProductFeed />
       </main>
     </div>
   );
