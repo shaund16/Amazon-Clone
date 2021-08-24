5ad9514ff659e7972c5858e14bdda768661e85cf
Shaun <s-purslow@hotmail.com>
2021-08-24 14:39:23 -0600

    add firebase file and store the config file from firebase and hide api key

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "clone-7b997.firebaseapp.com",
+  projectId: "clone-7b997",
+  storageBucket: "clone-7b997.appspot.com",
+  messagingSenderId: "264848646527",
+  appId: "1:264848646527:web:b2d9283771696a08493a4a"
+};
