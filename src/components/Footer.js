cf68a12160834071a76fa1445151fefccf63dc72
Shaun <s-purslow@hotmail.com>
2021-08-24 10:43:31 -0600

    add footer and columns and style it

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='space-x-3 p-4 pl-6 bg-amazon_blue-light text-white text-sm text-center mt-4 hover:bg-gray-700'>Back to top</div>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-amazon_blue text-center'>
+      <div className='space-y-4 text-xs text-white'>
+        <h5 className='font-bold text-sm'>Get to know us</h5>
+        <p>How Amazon Works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Amazon Prime</p>
+      </div>
+      <div className='space-y-4 text-xs text-white'>
+        <h5 className='font-bold text-sm '>Make Money with Us</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>A sweet clone</p>
+        <p>Referrals accepted</p>
+        <p></p>
+      </div>
+      <div className='space-y-4 text-xs text-white'>
+        <h5 className='font-bold text-sm'>Amazon Payment Products</h5>
+        <p>Shaun</p>
+        <p>FullStack</p>
+        <p>Investors</p>
+        <p>React</p>
+        <p>Next</p>
+      </div>
+      <div className='space-y-4 text-xs text-white '>
+        <h5 className='font-bold text-sm'>Let Us Help You</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>loreum</p>
+        <p>For the Win</p>
+        <p>Challenge</p>
+      </div>
+    </div>
+
+    </div>
+ 
+  );
+};
+
+export default Footer;
