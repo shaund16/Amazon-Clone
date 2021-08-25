45eec555b8a0c84c36c5d6df1191a8410c462837
Shaun <s-purslow@hotmail.com>
2021-08-25 14:00:46 -0600

    add next js public key variable to loadStripe

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,7 +6,7 @@ import { selectItems, selectTotal } from '../slices/basketSlice';
 import Currency from 'react-currency-formatter';
 import { useSession } from 'next-auth/client';
 import { loadStripe } from '@stripe/stripe-js';
-const stripePromise = loadStripe()
+const stripePromise = loadStripe(process.env.stripe_public_key)
 
 const Checkout = () => {
   const items = useSelector(selectItems);
